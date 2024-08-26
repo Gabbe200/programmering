60d2b2c2bad3c0f002e196fe0a1ebc608610d099
Gabbe200 <gabriel.forsberg@elev.ga.ntig.se>
2024-08-26 14:56:43 +0200

    bghgh

---
+++ modul0.py
@@ -0,0 +1,46 @@
+#välkomen till python
+
+"""hejsanplejsan
+Dettaär ett Blankalock av kommentarer """
+
+print("GaBlankariel") #inom citattecken
+
+#sträng/ strong variaBlankael
+name = "GaBlankariel"
+print(name)
+
+#integer /nummer varialBlankael (heltal)
+my_age = 16
+print(my_age)
+
+
+print((name) + "är" + str(my_age) + "år gammal")
+#float
+
+print("här är ett citat: \"jag är glad\"")
+
+print("citat \" Lorem Ipsum \" ")
+
+print("""
+Drottning Blanka
+Drottning Blanka
+Drottning Blanka
+Drottning Blanka
+Drottning Blanka
+Drottning Blanka
+Drottning Blanka
+Drottning Blanka
+Drottning Blanka
+Drottning Blanka
+Drottning Blanka
+""")
+
+print ("=" * 60) #enstaka rad av kommentar
+
+
+name = input ("skriv namn") #input är altid standard text (stäng)
+
+print (" välkommen \"" + name + "\"")
+
+print ("Gabriel\nForsberg")
+
