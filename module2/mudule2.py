70ec2c77b99a6f87616f4f289d12b6a1d92d0361
Gabbe200 <gabriel.forsberg@elev.ga.ntig.se>
2024-09-02 14:59:04 +0200

    mudule2

---
+++ module2/mudule2.py
@@ -0,0 +1,30 @@
+x = 3
+y = 2
+
+
+
+print(x//y)
+print(x%y)
+print(x ** y)
+
+
+name = input("vad heter du?: ")
+age = input("skriv din ålder: ")
+
+print("hejsan" , name , "du är cirka", age ,)
+
+x = int(input("x:"))
+y = int(input("y:"))
+
+print("svar", x * y)
+
+weight = float(input("vikt?:"))
+hight = float(input("längd?:"))
+
+print("BMI:", weight / hight ** 2)
+
+weeks = int(input("hur gammal är du:"))
+print("DU ÄR cirka", weeks * 52, "veckor gammal")
+
+kg = float(input("skriv kg:"))
+print(kg, "kg är", kg * 2.20462262, "lbs")
